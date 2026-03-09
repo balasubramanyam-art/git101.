18ad32e92c3931c66ea0b2e745dde0d47a83d816
Gitea1230 <marimuthubalasubramanyam.msm@gmail.com>
2026-03-09 10:33:39 +0530

    Second Change

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,11 @@
-cala
+a = int(input("Enter 1st number"))
+b = int(input("Enter 2nd number"))
+print("Enter your choice")
+if(choice == '+')
+print("Calculator App")
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+
