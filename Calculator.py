871d53cc2f4dcfbf3ae9b4fc86a1c4a999ab4fbc
balasubramanyam-art <balasubramanyam.msm@gmail.com>
2026-03-09 10:39:48 +0530

    Implement basic calculator operations
    Fix calculator logic to handle operations based on user choice.

--- Calculator.py
+++ Calculator.py
@@ -2,10 +2,15 @@ a = int(input("Enter 1st number"))
 b = int(input("Enter 2nd number"))
 print("Enter your choice")
 if(choice == '+')
+   result =a+b
 print("Calculator App")
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
+if(choice == '-')
+   result =a-b
+if (choice == '*')
+   result =a*b
+if (choice == '/')
+   result =a/b
+print(result)
+
 
 
